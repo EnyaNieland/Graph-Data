0acaa6b1e7e291c674d0ae10061c07211405ff7f
EnyaNieland <enya.nieland@gmail.com>
2024-02-26 11:34:54 +0100

    Setup: return sample data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,48 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const sampleData = {
+  "data":[
+    {
+      "name":"A",
+      "description":"This is a description of A",
+      "parent":""
+    },
+    {
+      "name":"B",
+      "description":"This is a description of B",
+      "parent":"A"
+    },
+    {
+      "name":"C",
+      "description":"This is a description of C",
+      "parent":"A"
+    },
+    {
+      "name":"D",
+      "description":"This is a description of D",
+      "parent":"A"
+    },
+    {
+      "name":"B-1",
+      "description":"This is a description of B-1",
+      "parent":"B"
+    },
+    {
+      "name":"B-2",
+      "description":"This is a description of B-2",
+      "parent":"B"
+    },
+    {
+      "name":"B-3",
+      "description":"This is a description of B-3",
+      "parent":"B"
+    }
+  ]
+};
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.json(sampleData);
 });
 
 app.listen(port, () => {
