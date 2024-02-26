08cdef42e29273aecb62a79fea1f5483f3170840
EnyaNieland <enya.nieland@gmail.com>
2024-02-26 15:28:34 +0100

    Change body to contain sample data

--- api/data/index.ts
+++ api/data/index.ts
@@ -1,17 +1,53 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const sampleData = {
+  data: [
+    {
+      name: "A",
+      description: "This is a description of A",
+      parent: "",
+    },
+    {
+      name: "B",
+      description: "This is a description of B",
+      parent: "A",
+    },
+    {
+      name: "C",
+      description: "This is a description of C",
+      parent: "A",
+    },
+    {
+      name: "D",
+      description: "This is a description of D",
+      parent: "A",
+    },
+    {
+      name: "B-1",
+      description: "This is a description of B-1",
+      parent: "B",
+    },
+    {
+      name: "B-2",
+      description: "This is a description of B-2",
+      parent: "B",
+    },
+    {
+      name: "B-3",
+      description: "This is a description of B-3",
+      parent: "B",
+    },
+  ],
+};
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: sampleData,
+  };
 };
 
 export default httpTrigger;
